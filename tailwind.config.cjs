22c02f2bba296e3e5d06078af097a84ae281ad25
Gifairu <gifairu@gmail.com>
2022-01-06 01:04:18 +0700

    Adding font and colors base

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,27 @@
 const config = {
+	mode: 'jit',
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				montserrat: ['Montserrat', 'sans-serif']
+			},
+			colors: {
+				base: {
+					50: '#ffe6b4',
+					100: '#ffe0a1',
+					200: '#ffd98d',
+					300: '#ffd37a',
+					400: '#ffcc66',
+					500: '#ffc552',
+					600: '#ffbf3f',
+					700: '#ffb82b',
+					800: '#ffb218',
+					900: '#ffab04'
+				}
+			}
+		}
 	},
-
 	plugins: []
 };
 
