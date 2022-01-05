8b50c9db12d0d9953b9586550ca9a4b3281fd7bf
Gifairu <gifairu@gmail.com>
2022-01-06 01:04:44 +0700

    Add navbar components

---
+++ src/lib/components/navbar/Navbar.svelte
@@ -0,0 +1,22 @@
+<div
+	class="fixed top-0 z-10 px-11 py-4 w-full border-b border-gray-900/10 backdrop-filter backdrop-blur-lg bg-white bg-opacity-95 font-montserrat"
+>
+	<div class="flex justify-between items-center">
+		<div class="flex justify-start items-center space-x-2 w-1/3">
+			<span class="hidden md:inline"><p class="text-gray-800">AQWorld Collections</p></span>
+		</div>
+		<div class="w-1/3">
+			<input
+				class="px-6 py-2 w-full text-sm rounded-sm bg-gray-300/20 outline outline-1 outline-gray-200 hover:outline-base-400 hover:shadow-md focus:outline-base-400 focus:shadow-md"
+				placeholder="Search the items"
+			/>
+		</div>
+		<div class="flex justify-end w-1/3">
+			<ul class="flex space-x-8 text-sm font-semibold leading-6 text-gray-700">
+				<li><a class="hover:text-base-400" href="/">Home</a></li>
+				<li><a class="hover:text-base-400" href="/items">Items</a></li>
+				<li><a class="hover:text-base-400" href="/">About</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
