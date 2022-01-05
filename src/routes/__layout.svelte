bfe97a6bb3c5790adf16f688554a6d7ff002e12f
Gifairu <gifairu@gmail.com>
2022-01-06 01:05:18 +0700

    Add component and tailwind css

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,2 +1,13 @@
-<script>import "../app.css";</script>
-<slot></slot>
+<script lang="ts">
+	// Component Import
+	import Navbar from '$lib/components/navbar/Navbar.svelte';
+
+	// TailwindCSS
+	import '../app.css';
+</script>
+
+<Navbar />
+
+<main>
+	<slot />
+</main>
